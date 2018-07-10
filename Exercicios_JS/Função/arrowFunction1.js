e2753f763a0ed0e7f0839baea724a77433e3ed2d
Lucas <lucas.afsantos@hotmail.com>
2018-07-10 13:19:16 -0300

    AA

---
+++ Exercicios_JS/Função/arrowFunction1.js
@@ -0,0 +1,15 @@
+let dobro = function(a){
+    return a*2;
+}
+
+console.log(dobro(2));
+
+const triplo = (a)=>{
+    return a*3;
+}
+
+console.log(triplo(3));
+
+const quadruplo = a => a*4;
+
+console.log(quadruplo(2));
