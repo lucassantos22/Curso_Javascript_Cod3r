a7c4d80467546053e5d68638ec1938632bc5662d
lucas.figueredo <lucas.figueredo@digitro.com.br>
2018-08-21 13:31:39 -0300

    Commitado

---
+++ Exercicios_JS/Objeto/oo.js
@@ -0,0 +1,18 @@
+// Código não executável
+
+// Procedural
+
+processamento(valor1,valor2,valor3)
+
+// OO 
+
+Objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+Objeto.processamento() // Foco passou a ser o objeto, antes era a função.
