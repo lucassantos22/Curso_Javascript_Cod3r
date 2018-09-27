33c9311283220c729c0d9d83e93be3955d2dad8e
Lucas Santos <lucas.afsantos@hotmail.com>
2018-09-26 23:06:44 -0300

    Fazendo aula de array e foreach1

---
+++ Exercicios_JS/Array/foreach1.js
@@ -0,0 +1,9 @@
+const aprovados = ["Lucas","Bárbara","Letícia","Kaio"];
+aprovados.forEach((el,indice,array)=>{
+    console.log(`${indice+1} -> ${el}`);
+    console.log(array);
+});
+
+aprovados.forEach(el=>{
+    console.log(el);
+})
