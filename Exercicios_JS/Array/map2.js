7b7f840e18d86c3dd20c25d59d8d9d976ba78313
Lucas Santos <lucas.afsantos@hotmail.com>
2018-09-29 21:52:57 -0300

    Map 2 feito

---
+++ Exercicios_JS/Array/map2.js
@@ -0,0 +1,18 @@
+const carrinho = [
+    '{"nome":"Borracha","preco":3.45}',
+    '{"nome":"Caderno","preco":33.90}',
+    '{"nome":"Kit de lapis","preco":41.22}',
+    '{"nome":"Caneta","preco":7.50}' 
+]
+
+// Retornar um array apenas com os preços
+
+const JSONParser = carrinho.map((el)=>{
+    return JSON.parse(el);
+});
+
+const apenasPreco = JSONParser.map(el=>{
+    return el.preco;
+});
+
+console.log(apenasPreco);
