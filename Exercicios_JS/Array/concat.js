3d3ebf016e840602824fe1d164cff2c41d951ef8
Lucas Santos <lucas.afsantos@hotmail.com>
2018-10-07 23:21:50 -0400

    z

---
+++ Exercicios_JS/Array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ["Ana","Alexandra"];
+const filhos = ["Daniel","Noah"];
+
+const totalFilhos = filhas.concat(filhos, "Jorge");
+console.log(totalFilhos, filhas, filhos);
+
+console.log([].concat([1,2],[3,4],[5,6]),[7,8]);
