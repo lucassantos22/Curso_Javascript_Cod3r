8c8e2e609fdf547c5bdb4f7076fe771d69d09289
Lucas Santos <lucas.afsantos@hotmail.com>
2018-10-14 00:20:16 -0300

    Aurrr

--- Exercicios_JS/node.js/pastaA/pastaB/acessarModulo.js
+++ Exercicios_JS/node.js/pastaA/pastaB/acessarModulo.js
@@ -1,3 +1,11 @@
 const moduloA = require('../../moduloA'); // Se eu botasse 'moduloa' funcionaria no MAC, Windows e não no Linux.
+const ola = require('saudacao').ola;
 
 console.log(moduloA.ateLogo)
+console.log(ola);
+
+const http = require('http');
+http.createServer((req,res)=>{
+    res.write("Bom dia!");
+    res.end()
+}).listen(3000);
