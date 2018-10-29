23bbe7926a0ae42dcba6c18f05da3c47f3aaf612
lucas.figueredo <lucas.figueredo@digitro.com.br>
2018-10-29 11:07:44 -0300

    Teste pessoal em relação à exportação de atributos entre módulos no node.js

---
+++ Exercicios_JS/PersonalTests/testeCliente.js
@@ -0,0 +1,7 @@
+const olaMundo = require('./testeModulo.js').frase;
+const olaMundo2 = require('./testeModulo.js').frase2;
+const olaMundo3 = require('./testeModulo.js').frase3;
+
+console.log(olaMundo);
+console.log(olaMundo2);
+console.log(olaMundo3);
---
+++ Exercicios_JS/PersonalTests/testeModulo.js
@@ -0,0 +1,7 @@
+module.exports = {
+    frase:"Olá mundo!"
+}
+
+module.exports.frase2 = "Olá mundo 2!";
+
+exports.frase3 = "Olá mundo 3!";
