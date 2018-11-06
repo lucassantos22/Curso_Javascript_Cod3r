fc44354f1d2721768d259e71bb10a86ecc25f19f
Lucas Santos <lucas.afsantos@hotmail.com>
2018-11-05 22:57:07 -0200

    Temporizador com Node-Schedule

--- Exercicios_JS/node.js/temporizador.js
+++ Exercicios_JS/node.js/temporizador.js
@@ -2,7 +2,7 @@ schedule = require('node-schedule');
 
 const tarefa1 = schedule.scheduleJob("*/5 * 22 * * 1", ()=>{
     console.log("Exetucando tarefa1: ", new Date().getTime());
-}) // A cada 5 minutos, Minutos das horas ,22 horas, Dia do mês, Mês, Dia da semana (0 - Domingo, 6 - Sabado)
+}) // A cada 5 segundos, Minutos das horas ,22 horas, Dia do mês, Mês, Dia da semana (0 - Domingo, 6 - Sabado)
 
 
 // Temporizador padrão do JS
@@ -10,7 +10,6 @@ setTimeout(()=>{
     tarefa1.cancel();
     console.log("Cancelando tarefa1");
 }, 20000)
-
 
 const regra = new schedule.RecurrenceRule();
 regra.dayOfWeek = new schedule.Range(1,5);
